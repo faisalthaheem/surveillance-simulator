c3e5e8d949ba8b84e47faa4f96e757aafe98b396
faisalthaheem <10938481+faisalthaheem@users.noreply.github.com>
2021-06-22 22:19:33 +0200

    fixed model paths added asphalt material to base

--- run-gazebo-hd.sh
+++ run-gazebo-hd.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export SDF_PATH=$PWD/..
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/.gazebo/models:$PWD/models
 roslaunch surveillance_simulator gazebo.launch world:=sim_world_hd.world
--- run-gazebo.sh
+++ run-gazebo.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 export SDF_PATH=$PWD/..
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:~/.gazebo/models:$PWD/models
 roslaunch surveillance_simulator gazebo.launch
